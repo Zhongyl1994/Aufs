b8c0e4fdfd73251d1ef083e543af9acbf7d60cf3
ZHANGLE996 <18018961841@163.COM>
2022-08-31 10:50:48 +0800

    hellofs 取消注册hellofs

--- hello.c
+++ hello.c
@@ -2,13 +2,18 @@
  * File            : inode.c
  * Author          : ZhangLe
  * CreateTime      : 2022-08-30 21:13:08
- * LastModified    : 2022-08-31 10:07:43
+ * LastModified    : 2022-08-31 10:42:54
  * Vim             : ts=4, sw=4
  */
 
-#include "hello.h"
-#include <linux/slab.h>
 #include <linux/module.h>
+#include "hello.h"
+#include <linux/buffer_head.h>
+#include <linux/slab.h>
+#include <linux/init.h>
+#include <linux/highuid.h>
+#include <linux/vfs.h>
+#include <linux/writeback.h>
 
 static struct kmem_cache * hello_inode_cachep;
 
@@ -31,6 +36,17 @@ static int init_inodecache(void)
     return 0;
 }
 
+static void destroy_inodecache(void)
+{
+    /*
+     * Make sure all delayed rcu free inodes are fulshed before
+     * we destory cache.
+     */
+
+    rcu_barrier();
+    kmem_cache_destroy(hello_inode_cachep);
+}
+
 static struct file_system_type hello_fs_type = {
     .owner = THIS_MODULE,
     .name = "hello",
@@ -44,7 +60,12 @@ static int __init init_hello_fs(void)
 
     if (err)
         goto out1;
+    err = register_filesystem(&hello_fs_type);
+    if (err)
+        goto out;
     return 0;
+out:
+    destroy_inodecache();
 out1:
     return err;
 }
@@ -52,6 +73,7 @@ out1:
 static void __exit exit_hello_fs(void)
 {
     unregister_filesystem(&hello_fs_type);
+    destroy_inodecache();
 }
 
 module_init(init_hello_fs);
